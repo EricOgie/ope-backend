c4c5b9b2e9264519f9ff5aa9ba6dcf4175c35548
Ericogia <ogia.aghahowa@gmail.com>
2021-11-09 17:53:11 +0100

    Cors log

--- app/handlers/corsmanager.go
+++ app/handlers/corsmanager.go
@@ -1,9 +1,19 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/EricOgie/ope-be/logger"
+)
 
 func ManageCors(res *http.ResponseWriter) {
+	logger.Info("CORS Place1")
 	(*res).Header().Set("Access-Control-Allow-Origin", "*")
 	(*res).Header().Set("Access-Control-Allow-Methods", "POST, GET, PATCH, PUT")
 	(*res).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+	hed := (*res).Header()
+	logger.Info("CORS Place2")
+	fmt.Println(fmt.Sprintf("%#v", hed))
+
 }
