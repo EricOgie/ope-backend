546b3d8f9dd1c0854e80d94190d6d31acaf173d1
Ericogia <ogia.aghahowa@gmail.com>
2021-11-09 21:33:21 +0100

    Lat

--- utils/config.go
+++ utils/config.go
@@ -37,7 +37,7 @@ type Config struct {
 // It returns a Config struct with all loaded envs as attributes.
 // Each env can then be accessed by the DOT notation on the Config struct like so: config.DBAddress
 func LoadConfig(path string) Config {
-
+	logger.Info("WHERE = " + os.Getenv("APP_ENV"))
 	if os.Getenv("APP_ENV") == konstants.ENV_PROD {
 		logger.Error("ENV = " + konstants.ENV_PROD)
 		config := getConfigFromOs()
